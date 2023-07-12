56aa999f0c54b87756f717d715fc320feb24c609
DevProsper <prosperngouari38@gmail.com>
2023-07-12 22:28:50 +0100

    Armonisation du template

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ASNEC-IT SCHOOL</title>
+
+    <link rel="stylesheet" 
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="{{asset('assets/adminlte/css/adminlte.min.css')}}">
+    <script nonce="1e447ae5-ff74-4740-9655-7bb597cc2dc0">
+        (function(w,d){!function(f,g,h,i){f[h]=f[h]||{};f[h].executed=[];f.zaraz={deferred:[],listeners:[]};f.zaraz.q=[];f.zaraz._f=function(j){return function(){var k=Array.prototype.slice.call(arguments);f.zaraz.q.push({m:j,a:k})}};for(const l of["track","set","debug"])f.zaraz[l]=f.zaraz._f(l);f.zaraz.init=()=>{var m=g.getElementsByTagName(i)[0],n=g.createElement(i),o=g.getElementsByTagName("title")[0];o&&(f[h].t=g.getElementsByTagName("title")[0].text);f[h].x=Math.random();f[h].w=f.screen.width;f[h].h=f.screen.height;f[h].j=f.innerHeight;f[h].e=f.innerWidth;f[h].l=f.location.href;f[h].r=g.referrer;f[h].k=f.screen.colorDepth;f[h].n=g.characterSet;f[h].o=(new Date).getTimezoneOffset();if(f.dataLayer)for(const s of Object.entries(Object.entries(dataLayer).reduce(((t,u)=>({...t[1],...u[1]})),{})))zaraz.set(s[0],s[1],{scope:"page"});f[h].q=[];for(;f.zaraz.q.length;){const v=f.zaraz.q.shift();f[h].q.push(v)}n.defer=!0;for(const w of[localStorage,sessionStorage])Object.keys(w||{}).filter((y=>y.startsWith("_zaraz_"))).forEach((x=>{try{f[h]["z_"+x.slice(7)]=JSON.parse(w.getItem(x))}catch{f[h]["z_"+x.slice(7)]=w.getItem(x)}}));n.referrerPolicy="origin";n.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(f[h])));m.parentNode.insertBefore(n,m)};["complete","interactive"].includes(g.readyState)?zaraz.init():f.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,"zarazData","script");})(window,document);
+    </script>
+    @yield('styles')
+
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+
+        @include("partials.topbar")
+
+
+        @include("partials.menu")
+
+        <div class="content-wrapper">
+
+            @yield("contenu")
+
+        </div>
+
+
+        @include("partials.profile")
+
+
+        <footer class="main-footer">
+            <small>Copyright &copy; 2021-2023 <a href="https://www.asnec-it.com">ASNEC-IT.COM</a>. 
+                Tous droits reservés.</small>
+        </footer>
+    </div>
+
+
+
+    <script src="{{asset('assets/adminlte/js/jquery.min.js')}}"></script>
+    
+    <script src="{{asset('assets/adminlte/js/bootstrap.bundle.min.js')}}"></script>
+    
+    <script src="{{asset('assets/adminlte/js/adminlte.min.js')}}"></script>
+
+    @yield('scripts')
+</body>
+
+</html>
---
+++ resources/views/modules/utilisateurs/index.blade.php
@@ -0,0 +1,5 @@
+@extends("layouts.master")
+
+@section("contenu")
+Page de test
+@endsection
